273ea6820286baafc6000b5448a90a4117c281d1
Anthonyuff <anthonyrebello612@gmail.com>
2026-03-11 18:07:49 -0300

    update scrip

--- eq1d.py
+++ eq1d.py
@@ -5,7 +5,7 @@ dt=0.002
 dz=5
 nz=500
 nt=500
-c=1500
+c=1000
 modelo=np.full(nz,c)
 tempo=np.arange(0,nt*dt,dt)
 prof= np.arange(0,nz*dz,dz)
@@ -15,16 +15,16 @@ P= np.zeros((nz,nt))
 def ricker(t):
   f_corte = 30
   fc = f_corte / (3 * np.sqrt(np.pi))
-  td = t - (2 * np.sqrt(np.pi) / fc)
+  td = t - (0.5 * np.sqrt(np.pi) / fc)
   arg = np.pi * (np.pi * fc * td)**2
   return (1 - 2*arg) * np.exp(-arg)
 
 ricker2= ricker(tempo)
 
-def eq1d(P,dt,dz,modelo,nt,nz):
-  sId = int(prof[250]/ dz)
+def eq1d(P,dt,dz,nt,nz):
+  #sId = int(234/ dz)
   for i in range(1,nt-1):
-    P[sId,i] += ricker2[i]
+    P[250,i] += ricker2[i]
     for n in range(1,nz-1):
         laplacian=	(1*P[n-1,i]-2*P[n+0,i]+1*P[n+1,i])/(1*1.0*dz**2)
 
@@ -32,7 +32,7 @@ def eq1d(P,dt,dz,modelo,nt,nz):
 
   return P
 
-P=eq1d(P,dt,dz,modelo,nt,nz)
+P=eq1d(P,dt,dz,nt,nz)
 
 from matplotlib.animation import FuncAnimation
 
@@ -46,5 +46,5 @@ def atualizar(frame):
     ax.set_title(f"Tempo = {frame*dt:.3f} s")
     return linha,
 
-ani = FuncAnimation(fig, atualizar, frames=range(50,nt), interval=50)
+ani = FuncAnimation(fig, atualizar, frames=nt, interval=50)
 plt.show()
