d6344a8a0aabae02631b9a970ae8dbd994dc1ba7
Anthonyuff <anthonyrebello612@gmail.com>
2026-03-22 23:08:28 -0300

    add rec

--- eq1d.py
+++ eq1d.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 dt=0.001
 dz=5
 nz=500
-nt=500
+nt=3200
 c=1000
 modelo=np.full(nz,c)
 tempo=np.arange(0,nt*dt,dt)
@@ -19,39 +19,65 @@ def ricker(t):
   arg = np.pi * (np.pi * fc * td)**2
   return (1 - 2*arg) * np.exp(-arg)
 
-ricker2= ricker(tempo)
+# def ricker3(t):
+#   f_corte = 40
+#   fc = f_corte / (3 * np.sqrt(np.pi))
+#   td = t - (0.5 * np.sqrt(np.pi) / fc)
+#   arg = np.pi * (np.pi * fc * td)**2
+#   return (1 - 2*arg) * np.exp(-arg)
 
-def eq1d(P,dt,dz,nt,nz):
+ricker2= ricker(tempo)
+# ricker4= ricker3(tempo)
+rec_pos = [50,200]
+nrec = len(rec_pos)
+
+rec = np.zeros((nt, nrec))
+def eq1d(P,dt,dz,nt,nz,rec_pos,rec):
   #sId = int(234/ dz)
+  nrec = len(rec_pos)
   for i in range(1,nt-1):
     P[250,i] += ricker2[i]
+    # P[300,i] += ricker4[i]
     for n in range(1,nz-1):
         laplacian=	(1*P[n-1,i]-2*P[n+0,i]+1*P[n+1,i])/(1*1.0*dz**2)
 
         P[n,i+1] = (c*dt)**2 * laplacian + 2*P[n,i] - P[n,i-1]
+    for j in range(nrec):
+            rec[i,j] = P[rec_pos[j], i]
 
-  return P
+  return P,rec
 
-P=eq1d(P,dt,dz,nt,nz)
+P,rec=eq1d(P,dt,dz,nt,nz,rec_pos,rec)
 
-plt.imshow(P.T, aspect='auto', cmap='gray',
+plt.imshow(rec.T, aspect='auto', cmap='gray',
            extent=[0, nt*dt, nz*dz, 0])
 
 plt.xlabel('time (s)')
 plt.ylabel('death (m)')
 plt.show()
+
+t = np.arange(nt)*dt
+
+for j in range(2):
+    plt.plot(t, rec[:,j], label=f"rec {rec_pos[j]}")
+
+plt.xlabel("time (s)")
+plt.ylabel("amplitude")
+plt.legend()
+plt.show()
+
 from matplotlib.animation import FuncAnimation
 
 fig,ax = plt.subplots()
 linha, = ax.plot(prof, P[:,0])
 ax.set_xlim(prof.min(), prof.max())
-ax.set_ylim(-1.5, 1.5)
+ax.set_ylim(-P.max(),P.max() )
 
 def atualizar(frame):
     linha.set_ydata(P[:,frame])
     ax.set_title(f"time = {frame*dt:.3f} s")
     return linha,
 
-ani = FuncAnimation(fig, atualizar, frames=nt, interval=50)
-ani.save('onda.gif',writer='pilow',fps=30)
+ani = FuncAnimation(fig, atualizar, frames=nt, interval=20)
+#ani.save('onda.gif',writer='pilow',fps=30)
 plt.show()
--- eq2d.py
+++ eq2d.py
@@ -1,16 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-dt=0.001
-dz=5
-nz=500
-nt=1000
-nx=500
-dx=5
-c=1000
+# dt=0.001
+# dz=5
+# nz=500
+# nt=1000
+# nx=500
+# dx=5
+# c=1000
 
 
-tempo=np.arange(0,nt*dt,dt)
+# tempo=np.arange(0,nt*dt,dt)
 
-prof= np.arange(0,nz*dz,dz)
+# prof= np.arange(0,nz*dz,dz)
 
-offset=np.arange(0,nx*dx,dx)
+# offset=np.arange(0,nx*dx,dx)
+
+r = 5
+
+theta = np.linspace(0, 2*np.pi, 1000)
+
+x = r*np.cos(theta)
+y = r*np.sin(theta)
+
+plt.plot(x,y)
+   # deixa o círculo correto
+plt.show()
